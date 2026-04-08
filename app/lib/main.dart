12b9ed43f42e83e34e3c41ddbd4492b5152b0571
VincentGouygou <vincent.gouygou@gmx.fr>
2026-04-08 23:15:05 +0200

    3

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+ 
 import 'package:flutter/material.dart';
 //import 'package:menu_bar/menu_bar.dart';
 void main() {
@@ -13,21 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter App 1',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        
         colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 60, 202, 24)),
       ),
       home:  const App1(title: 'Flutter App 1 '),
@@ -37,25 +24,17 @@ class MyApp extends StatelessWidget {
 
 class App1 extends StatefulWidget {
   const App1({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  
   final String title;
-
+ 
   @override
   State<App1> createState() => _App1State();
 }
 
 class _App1State extends State<App1> {
-   
-
+   final bool _isConnected = true;
+   final String _name="Vincent";
+    
   void _functionA() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -87,10 +66,15 @@ class _App1State extends State<App1> {
           title: Center( 
               child: Text(widget.title),            
             ),
+          leadingWidth: 100,
           leading: Center(
               child: ElevatedButton(
-                onPressed: _App1State.new, 
-                child: Text('Connect') ,
+                onPressed: () {
+
+                }, 
+                
+                child: _isConnected ? Text('$_name') : Text('Login' ) ,
+             
               ) 
             ),
           actions: <Widget> [
